f7c52c00cf2408add784bc7c728945db5cebda36
davis <davisliam123@gmail.com>
2022-02-10 12:53:55 -0800

    K Means Clustering finished product

--- src/Filters/ColorMasking.java
+++ src/Filters/ColorMasking.java
@@ -2,17 +2,20 @@ package Filters;
 
 import Interfaces.PixelFilter;
 import core.DImage;
+import processing.core.PApplet;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class ColorMasking implements PixelFilter {
-    private int rVal, bVal,gVal, threshold;
+    private int colorVal, otherVal, threshold;
+    public static ArrayList<Point> objectPixels;
 
     public ColorMasking(){
-        rVal = 160;
-        gVal = 0;
-        bVal = 0;
+        colorVal = 200;
+        otherVal = 0;
         threshold = 100;
+        objectPixels = new ArrayList<>();
     }
 
 
@@ -29,8 +32,10 @@ public class ColorMasking implements PixelFilter {
 
                 if(calculateDistance(redVal, greenVal, blueVal)<= threshold){
                     red[i][j] = 255;
-                    blue[i][j] = 255;
-                    green[i][j] = 255;
+                    blue[i][j] = 0;
+                    green[i][j] = 0;
+
+                    objectPixels.add(new Point(i, j));
                 }
                 else{
                     red[i][j] = 0;
@@ -40,6 +45,7 @@ public class ColorMasking implements PixelFilter {
             }
 
         }
+        System.out.println(calculateCenter(img));
         img.setColorChannels(red, green, blue);
         return img;
 
@@ -48,9 +54,44 @@ public class ColorMasking implements PixelFilter {
 
 
     public double calculateDistance(int r, int g, int b){
-        double distance = Math.sqrt( ((r-rVal)*(r-rVal)) + ((g-gVal)*(g-gVal))+ ((b-bVal)*(b-bVal)));
-        return distance;
+        return Math.sqrt( ((r-colorVal)*(r-colorVal)) + ((g-otherVal)*(g-otherVal))+ ((b-otherVal)*(b-otherVal)));
     }
 
+    public Point calculateCenter(DImage image){
+        int num = 0;
+        int rowCenter = 0;
+        int colCenter = 0;
+        int rowTotal = 0;
+        int colTotal = 0;
+        short[][] red = image.getRedChannel();
+        short[][] blue = image.getBlueChannel();
+        short[][] green = image.getGreenChannel();
+        for (int i = 0; i < red.length ; i++) {
+            for (int j = 0; j < red[0].length; j++) {
+                if(red[i][j] == 255){
+                    rowTotal+= i;
+                    num++;
+                    colTotal +=j;
 
+                }
+            }
+
+        }
+
+        if (num == 0) {
+            return new Point(-1, -1);
+        }
+
+        rowCenter = rowTotal/ (num);
+        colCenter = colTotal/(num);
+
+        Point center = new Point(colCenter, rowCenter);
+        return center;
+    }
+
+    public void drawOverlay(PApplet window, DImage original, DImage filtered) {
+
+
+    }
 }
+
--- src/Filters/KMeansClustering.java
+++ src/Filters/KMeansClustering.java
@@ -1,11 +1,143 @@
 package Filters;
 
+import Interfaces.Drawable;
 import Interfaces.PixelFilter;
 import core.DImage;
+import processing.core.PApplet;
 
-public class KMeansClustering implements PixelFilter {
+import java.awt.*;
+import java.util.ArrayList;
+
+public class KMeansClustering implements PixelFilter, Drawable {
+    private ArrayList<Point> allObjectPixels;
+    private ArrayList<Point> cluster1, cluster2, cluster3;
+    private boolean done;
+    
+    private Point cluster1Center, cluster2Center, cluster3Center;
+
+    public KMeansClustering() {
+        cluster1 = new ArrayList<>();
+        cluster2 = new ArrayList<>();
+        cluster3 = new ArrayList<>();
+
+        allObjectPixels = ColorMasking.objectPixels;
+        done = false;
+
+        // initialize random points
+        cluster1Center = allObjectPixels.get((int)(Math.random() * allObjectPixels.size()));
+        cluster2Center = allObjectPixels.get((int)(Math.random() * allObjectPixels.size()));
+        cluster3Center = allObjectPixels.get((int)(Math.random() * allObjectPixels.size()));
+    }
+
     @Override
     public DImage processImage(DImage img) {
-        return null;
+        createClusters(allObjectPixels);
+        return img;
+    }
+
+    private void createClusters(ArrayList<Point> objectPixels) {
+        do {
+            // clear data
+            cluster1.clear();
+            cluster2.clear();
+            cluster3.clear();
+
+            // initialize random points
+            cluster1Center = allObjectPixels.get((int)(Math.random() * allObjectPixels.size()));
+            cluster2Center = allObjectPixels.get((int)(Math.random() * allObjectPixels.size()));
+            cluster3Center = allObjectPixels.get((int)(Math.random() * allObjectPixels.size()));
+
+            // assign clusters
+            assignClusters(objectPixels);
+
+            // save centers
+            Point originalCluster1 = cluster1Center;
+            Point originalCluster2 = cluster2Center;
+            Point originalCluster3 = cluster3Center;
+
+
+            // recalculate clusters
+            reCalculateClusters();
+
+            // save recalculated centers and differences
+            Point nextCluster1 = cluster1Center;
+            Point nextCluster2 = cluster2Center;
+            Point nextCluster3 = cluster3Center;
+
+            double cluster1Movement = originalCluster1.distance(nextCluster1);
+            double cluster2Movement = originalCluster2.distance(nextCluster2);
+            double cluster3Movement = originalCluster3.distance(nextCluster3);
+
+            // evaluate done
+            if (cluster1Movement == 0 && cluster2Movement == 0 && cluster3Movement == 0) done = true;
+
+            System.out.println(cluster1Center + ", " + cluster2Center + ", " + cluster3Center);
+        } while (!done);
+    }
+
+    private void reCalculateClusters() {
+        // cluster 1
+        int cluster1x = 0;
+        int cluster1y = 0;
+
+        int cluster1size = cluster1.size();
+        if (cluster1size == 0) cluster1size = 1;
+
+        for (Point p : cluster1) {
+            cluster1x += p.getX();
+            cluster1y += p.getY();
+        }
+
+        cluster1Center = new Point(cluster1x / cluster1size, cluster1y / cluster1size);
+
+
+        // cluster 2
+        int cluster2x = 0;
+        int cluster2y = 0;
+
+        int cluster2size = cluster2.size();
+        if (cluster2size == 0) cluster2size = 1;
+
+        for (Point p : cluster2) {
+            cluster2x += p.getX();
+            cluster2y += p.getY();
+        }
+
+        cluster2Center = new Point(cluster2x / cluster2size, cluster2y / cluster2size);
+
+        // cluster 3
+        int cluster3x = 0;
+        int cluster3y = 0;
+
+        int cluster3size = cluster3.size();
+        if (cluster3size == 0) cluster3size = 1;
+
+        for (Point p : cluster3) {
+            cluster3x += p.getX();
+            cluster3y += p.getY();
+        }
+
+        cluster3Center = new Point(cluster3x / cluster3size, cluster3y / cluster3size);
+    }
+
+
+    private void assignClusters(ArrayList<Point> objectPixels) {
+        // iterate over all points and assign to center
+        for (Point p : objectPixels) {
+            double distanceCluster1 = p.distance(cluster1Center);
+            double distanceCluster2 = p.distance(cluster2Center);
+            double distanceCluster3 = p.distance(cluster3Center);
+
+            if (distanceCluster1 < distanceCluster2 & distanceCluster1 < distanceCluster3) cluster1.add(p);
+            else if (distanceCluster2 < distanceCluster1 && distanceCluster2 < distanceCluster3) cluster2.add(p);
+            else cluster3.add(p);
+        }
+    }
+
+    @Override
+    public void drawOverlay(PApplet window, DImage original, DImage filtered) {
+        window.ellipse(cluster1Center.x, cluster1Center.y, 5, 5);
+        window.ellipse(cluster2Center.x, cluster2Center.y, 5, 5);
+        window.ellipse(cluster3Center.x, cluster3Center.y, 5, 5);
     }
 }
